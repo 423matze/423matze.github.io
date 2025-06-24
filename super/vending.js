884ea36e755f7d8518ffad43629353121b936fe3
Matze Lenz <matze@423.ch>
2025-06-24 12:59:26 +0200

    Create vending.js - 0.1.0

---
+++ super/vending.js
@@ -0,0 +1,22 @@
+// rive prototype: 0.1.0
+// vending machine prototype
+
+function initVendingPrototype() {
+  const r = new rive.Rive({
+    src: "https://423matze.github.io/super/ttrays-003.riv", // Ensure this file name is correct
+    canvas: document.getElementById("rivecanvas"),
+    autoplay: true,
+    autoBind: true,
+    artboard: "vending",
+    stateMachines: "tray machine",
+
+    onLoad: () => {
+      r.resizeDrawingSurfaceToCanvas();
+    },
+    onLoadError: (e) => {
+      console.error("Rive error:", e);
+    },
+  });
+}
+// Ensure the canvas is ready before initializing
+requestAnimationFrame(initVendingPrototype);
